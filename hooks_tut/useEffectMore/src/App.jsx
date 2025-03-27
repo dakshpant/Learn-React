d82378f677ccc22188b4cbba348828505fbf33d8
Daksh Pant <dakshpant15@gmail.com>
2025-03-27 14:12:51 +0530

    "updated code"

--- hooks_tut/useEffectMore/src/App.jsx
+++ hooks_tut/useEffectMore/src/App.jsx
@@ -2,47 +2,73 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts')
 
+  // Example 1
+  const [resourceType, setResourceType] = useState('posts')
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    // console.log(resourceType);
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [resourceType])
+  // useEffect(() => {
+  //   // console.log(resourceType);
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then(response => response.json())
+  //     .then(json => setItems(json))
+  // }, [resourceType])
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+// ===================================================================================================================
 
-  const handelResize = () => {
-    setWindowWidth(window.innerWidth);
-  }
+  // Example 2
 
-  useEffect(() => {
-    window.addEventListener('resize', handelResize);
-  })
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // const handelResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handelResize);
+  // })
+
+  // ===================================================================================================================
+
+  // Example 3
+
+  useEffect(()=>{
+    console.log(`Resource Changed`);
+
+    //CleanUp Function
+    return () => {
+      console.log(`Return from resource change`);
+      
+    } 
+  },[resourceType])
+
+  // ===================================================================================================================
 
   return (
-    // <>
-    //   <div>
-    //     <button onClick={() => setResourceType('posts')}>Post</button>
-    //     <button onClick={() => setResourceType('users')}>User</button>
-    //     <button onClick={() => setResourceType('comments')}>Comments</button>
-    //   </div>
-    //   <h1>{resourceType}</h1>
-    //   {
-    //     items.map
-    //       (
-    //         items => {
-    //           return <pre>{JSON.stringify(items)}</pre>
-    //         }
-    //       )
-    //   }
-    // </>
-    <div>
-      Window Width : {windowWidth}
-    </div>
+
+    // Example 1 and 3
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Post</button>
+        <button onClick={() => setResourceType('users')}>User</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {
+        items.map
+          (
+            items => {
+              return <pre>{JSON.stringify(items)}</pre>
+            }
+          )
+      }
+
+{/* // =================================================================================================================== */}
+{/* Example 2 */}
+    </>
+    // <div>
+    //   Window Width : {windowWidth}
+    // </div>
   )
 }
 
